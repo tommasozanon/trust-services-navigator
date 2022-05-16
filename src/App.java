63fe7365ed63eabbaf23947070d0a3a68f305b9e
tommasozanon <tommymaso1@gmail.com>
2022-05-16 15:41:32 +0200

    prime prove

--- src/App.java
+++ src/App.java
@@ -1,32 +1,24 @@
+
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
+
 public class App extends Application {
+
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("prova.fxml"));
+        primaryStage.setTitle("prova");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
+
 }
