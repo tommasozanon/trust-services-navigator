ad5048fa141bd003f2bacd7c24c45e40e08dc146
tommasozanon <tommymaso1@gmail.com>
2022-05-16 22:53:16 +0200

    started the api part, implemented conversion to json (look at read me)

--- README.md
+++ README.md
@@ -16,3 +16,7 @@ Meanwhile, the compiled output files will be generated in the `bin` folder by de
 ## Dependency Management
 
 The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+
+Per lanciare il programma aggiungere i file jar scaricabili da questi link alle librerie:
+https://jar-download.com/artifacts/org.json
+https://gluonhq.com/products/javafx/
--- src/App.java
+++ src/App.java
@@ -6,10 +6,18 @@ import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.fxml.FXMLLoader;
 
+import java.io.IOException;
+
+import org.json.JSONObject;
+
 public class App extends Application {
 
-    public static void main(String[] args) {
-        launch();
+    public static void main(String[] args) throws IOException {
+        // launch();
+
+        SiteRequests sr = SiteRequests.getInstance();
+        System.out.println(sr.country_list());
+
     }
 
     @Override
