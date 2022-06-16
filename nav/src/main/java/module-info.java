2935a58bc8f6746006a106c31cf49404ae8fd228
tommasozanon <tommymaso1@gmail.com>
2022-06-16 12:13:25 +0200

    aa

--- nav/src/main/java/module-info.java
+++ nav/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.trust {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.json;
+    requires javafx.base;
+    requires javafx.graphics;
 
     opens com.trust to javafx.fxml;
 
